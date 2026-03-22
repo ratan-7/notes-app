11f87f903beebc796bb3536f87ecdc07f426fbf6
Ratan Mahata <ratanmahata116@gmail.com>
2026-03-23 01:00:54 +0530

    updated api url for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://notes-app-8w4q.onrender.com/"
 })
 
 API.interceptors.request.use((req) => {
