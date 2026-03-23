75f9a5888073be62709daa3cd45ebd652722cafe
Ratan Mahata <ratanmahata116@gmail.com>
2026-03-23 13:46:42 +0530

    fixed api baseURL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: "https://notes-app-8w4q.onrender.com/"
+    baseURL: "https://notes-app-8w4q.onrender.com/api"
 })
 
 API.interceptors.request.use((req) => {
